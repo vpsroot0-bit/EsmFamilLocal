0acf8b199e73a77cba2e100797b544de1e236459
vpsroot0-bit <vpsroot0@gmail.com>
2026-05-28 19:53:25 +0330

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,10 @@
+// Polyfills باید قبل از همه چیز import شوند
+import { Buffer } from 'buffer';
+import process from 'process';
+
+global.Buffer = Buffer;
+global.process = process;
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
